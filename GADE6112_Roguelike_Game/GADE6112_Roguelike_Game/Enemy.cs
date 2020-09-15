1a761b80c6f7f5100bc9ad05356c5d9996156ef9
Niknak52518 <40242343+Niknak52518@users.noreply.github.com>
2020-09-15 15:29:45 +0200

    Add files via upload

---
+++ GADE6112_Roguelike_Game/GADE6112_Roguelike_Game/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GADE6112_Roguelike_Game
+{
+    class Enemy : Character
+    {
+        Random r = new Random();
+
+        public Enemy(float x, float y, float damage, float hp, float maxhp) : base(x, y, damage, hp, maxhp)
+        {
+            this.posX = x;
+            this.posY = y;
+            this.Damage = damage;
+            this.HP = hp;
+        }
+
+        public override string ToString()
+        {
+            return "Enemy at [" + posX + "," + posY + "] (" + Damage + ")" ;
+        }
+    }
+}
