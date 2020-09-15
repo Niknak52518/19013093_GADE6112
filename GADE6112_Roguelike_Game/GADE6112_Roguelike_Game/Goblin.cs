fc90edfa298ffc88342f28901dba8cdca623ed64
Niknak52518 <40242343+Niknak52518@users.noreply.github.com>
2020-09-15 15:30:44 +0200

    Add files via upload

---
+++ GADE6112_Roguelike_Game/GADE6112_Roguelike_Game/Goblin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GADE6112_Roguelike_Game
+{
+    class Goblin : Enemy
+    {
+        public Goblin(float x, float y, float damage, float hp, float maxhp) : base(x, y, damage, hp, maxhp)
+        {
+            this.posX = x;
+            this.posY = y;
+            this.Damage = 1;
+            this.HP = 10;
+        }
+    }
+}
---
+++ GADE6112_Roguelike_Game/GADE6112_Roguelike_Game/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GADE6112_Roguelike_Game
+{
+    class Hero : Character
+    {
+        public Hero(float x, float y, float damage, float hp, float maxhp) : base(x, y, damage, hp, maxhp)
+        {
+            this.posX = x;
+            this.posY = y;
+            this.Damage = 2;
+            this.HP = hp;
+            this.MaxHP = maxhp;
+        }
+
+        public override string ToString()
+        {
+            return "Player Stats:\n" + "HP: " + HP + "/" + MaxHP + "\n" + "Damage: " + Damage + "\n" + "[" + posX + "," + posY + "]";
+        }
+    }
+}
