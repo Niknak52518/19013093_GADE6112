6bae8a7b3ffe8b9b3078bb919b2bf3a1ab74aa7c
Niknak52518 <40242343+Niknak52518@users.noreply.github.com>
2020-09-15 15:27:17 +0200

    Add files via upload

---
+++ GADE6112_Roguelike_Game/GADE6112_Roguelike_Game/Character.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GADE6112_Roguelike_Game
+{
+    public enum Movement
+    {
+        NO_MOVEMENT,
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+    public abstract class Character : Tile
+    {
+        protected float HP;
+        protected float MaxHP;
+        protected float Damage;
+        protected Tile[] tile;
+        public bool isDead;
+
+        public abstract int PosX { get; set; }
+        public abstract int PosY { get; set; }
+        public abstract string Symbol { get; }
+
+        public Character(float x, float y, float damage, float hp, float maxhp) : base(x, y, TileType.EMPTY_TILE)
+        {
+            this.posX = x;
+            this.posY = y;
+        }
+
+        public virtual void Attack()
+        {
+
+        }
+
+        public virtual bool CheckRange()
+        {
+            return false;
+        } 
+
+        private int DistanceTo()
+        {
+            return 0;
+        }
+
+        public void Move()
+        {
+
+        }
+
+        public abstract override string ToString();
+    }
+}
