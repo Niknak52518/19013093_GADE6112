705a4120594bdc4f5217edab3ec63390bc9a9195
Niknak52518 <40242343+Niknak52518@users.noreply.github.com>
2020-09-15 15:32:02 +0200

    Add files via upload

---
+++ GADE6112_Roguelike_Game/GADE6112_Roguelike_Game/GameEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GADE6112_Roguelike_Game
+{
+    class GameEngine
+    {
+        public Map field;
+        Random random = new Random();
+
+        public void GameLogic()
+        {
+            field = new Map(20);
+        }
+
+        public bool MovePlayer()
+        {
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ GADE6112_Roguelike_Game/GADE6112_Roguelike_Game/Map.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GADE6112_Roguelike_Game
+{
+    class Map
+    {
+        string[,] map = new string[20, 20];
+        Random random = new Random();
+        Character[] characters;
+        int counter;
+
+        public Map(int numOfCharacters)
+        {
+            characters = new Character[numOfCharacters];
+            SetCharacters();
+        }
+
+        private void SetCharacters()
+        {
+            for (int i = 0; i < characters.Length; i++)
+            {
+                counter = random.Next(0, 2);
+                if (counter == 0)
+                {
+                    Goblin goblin = new Goblin(1f, 1f, 1f, 1f, 1f);
+                    goblin.PosX = random.Next(0, 20);
+                    goblin.PosY = random.Next(0, 20);
+                    characters[i] = goblin;
+                }
+            }
+
+        }
+
+        public void SetMap()
+        {
+            for (int y = 0; y < 20; y++)
+            {
+                for (int x = 0; x < 20; x++)
+                {
+                    map[x, y] = "  .  ";
+                }
+            }
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                map[characters[i].PosX, characters[i].PosY] = characters[i].Symbol;
+            }
+        }
+
+        public void Display(Label lblMap)
+        {
+            lblMap.Text = "";
+            for (int y = 0; y < 20; y++)
+            {
+                for (int x = 0; x < 20; x++)
+                {
+                    lblMap.Text += map[x, y];
+                }
+                lblMap.Text += "\n";
+            }
+        }
+
+        public void DisplayInformation(RichTextBox txtInfoBox)
+        {
+            txtInfoBox.Text = "";
+            for (int i = 0; i < 5; i++)
+            {
+                txtInfoBox.Text += characters[i].ToString();
+
+            }
+        }
+    }
+}
